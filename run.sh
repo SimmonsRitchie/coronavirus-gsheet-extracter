dd9d7a6e96571a57c8488d36cdea364976fef13d
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 16:00:02 -0400

    Fix: Add python version

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ export PIPENV_DOTENV_LOCATION="/home/dansr/projects/coronavirus_gs_extracter/.en
 # Navigate to scraper project directory
 cd /home/dansr/projects/coronavirus_gs_extracter/
 # This is needed to ensure pipenv runs when used from cron
+export PYENV_VERSION="3.6.10"
 export PATH=~/.pyenv/shims:~/.pyenv/bin:"$PATH"
 # Run program
 pipenv run python coronavirus_gs_extracter.py
