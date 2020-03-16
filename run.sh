4798e25b6266bcaad3ce589715d42355e5cdefe7
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 15:04:58 -0400

    Fix: paths

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 # Cronjob script
 
 # This tells pipenv to use this .env file
-export PIPENV_DOTENV_LOCATION="/home/projects/dansr/coronavirus_gs_extracter/.env"
+export PIPENV_DOTENV_LOCATION="~/projects/dansr/coronavirus_gs_extracter/.env"
 # Navigate to scraper project directory
-cd /home/dansr/projects/coronavirus_gs_extracter/
+cd ~/dansr/projects/coronavirus_gs_extracter/
 # This is needed to ensure pipenv runs when used from cron
 PATH=/usr/local/bin:$PATH
 # Run program
