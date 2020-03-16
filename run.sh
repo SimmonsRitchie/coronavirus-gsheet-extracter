0388077b5d9b64d6da84ce014e15111871d4a726
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 17:18:55 -0400

    Testing

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ echo "New cronjob test"
 export PYENV_VERSION=3.6.10
 ~/.pyenv/bin/pyenv versions
 python --version
+which python
 # This tells pipenv to use this .env file
 export PIPENV_DOTENV_LOCATION="/home/dansr/projects/coronavirus_gs_extracter/.env"
 # Navigate to scraper project directory
 cd /home/dansr/projects/coronavirus_gs_extracter/
 # Run program
 pipenv run python coronavirus_gs_extracter.py
+# /home/dansr/.pyenv/shims/pipenv
