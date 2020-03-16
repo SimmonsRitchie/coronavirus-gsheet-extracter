803f00c2a6bbf427e3b353e80755240a0fedee06
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 17:09:34 -0400

    Testing

--- run.sh
+++ run.sh
@@ -2,27 +2,29 @@
 # Cronjob script
 
 # Start
-echo "Start cronjob"
-# Set paths
-export PYENV_ROOT=~/.pyenv
-export PYENV_VERSION=3.6.10
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:"$PATH"
-export PATH=~/.pyenv/shims:~/.pyenv/bin:"$PATH"
-export PATH="${HOME}/.pyenv/scripts:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-echo $PATH
+echo "New cronjob test"
+~/.pyenv/bin
 
-# Searching
-pyenv versions
-whereis pyenv
-whereis pipenv
-python --version
-# Start pyenv shell
-pyenv shell 3.6.10
-# This tells pipenv to use this .env file
-export PIPENV_DOTENV_LOCATION="/home/dansr/projects/coronavirus_gs_extracter/.env"
-# Navigate to scraper project directory
-cd /home/dansr/projects/coronavirus_gs_extracter/
-# Run program
-pipenv run python coronavirus_gs_extracter.py
+## Set paths
+#export PYENV_ROOT=~/.pyenv
+#export PYENV_VERSION=3.6.10
+#export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:"$PATH"
+#export PATH=~/.pyenv/shims:~/.pyenv/bin:"$PATH"
+#export PATH="${HOME}/.pyenv/scripts:$PATH"
+#eval "$(pyenv init -)"
+#eval "$(pyenv virtualenv-init -)"
+#echo $PATH
+#
+## Searching
+#pyenv versions
+#whereis pyenv
+#whereis pipenv
+#python --version
+## Start pyenv shell
+#pyenv shell 3.6.10
+## This tells pipenv to use this .env file
+#export PIPENV_DOTENV_LOCATION="/home/dansr/projects/coronavirus_gs_extracter/.env"
+## Navigate to scraper project directory
+#cd /home/dansr/projects/coronavirus_gs_extracter/
+## Run program
+#pipenv run python coronavirus_gs_extracter.py
