2ee6ef33998bf8ce5c5f78f295d401e0ad829538
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 18:04:25 -0400

    Another test

--- run.sh
+++ run.sh
@@ -2,22 +2,11 @@
 # Cronjob script
 
 # Start
-echo "New cronjob test"
-export PATH="~/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-export PYENV_VERSION=3.6.10
-~/.pyenv/bin/pyenv versions
-python --version
-which python
+printf "Cronjob run - Google sheet extract and move"
+printf -v date '%(%Y-%m-%d)T\n' -1
 # This tells pipenv to use this .env file
 export PIPENV_DOTENV_LOCATION="/home/dansr/projects/coronavirus_gs_extracter/.env"
 # Navigate to scraper project directory
 cd /home/dansr/projects/coronavirus_gs_extracter/
 # Run program
 ~/.pyenv/versions/3.6.10/bin/pipenv run python coronavirus_gs_extracter.py
-# /home/dansr/.pyenv/shims/pipenv
-
-#export PATH="/home/dansr/.pyenv/bin:$PATH"
-#eval "$(pyenv init -)"
-#eval "$(pyenv virtualenv-init -)"
