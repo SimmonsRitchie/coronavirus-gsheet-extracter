51a1a1bdc44c1a661e2339ec3f93ef81d9a9b889
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 11:17:43 -0400

    Correct assert error message

--- coronavirus_gs_extracter/download_sheet.py
+++ coronavirus_gs_extracter/download_sheet.py
@@ -6,7 +6,7 @@ def download_sheet(sheet_name, sheet_id, gid):
     s = requests.Session()
     google_sheet_url = f"https://docs.google.com/spreadsheets/d/e/{sheet_id}/pub?gid={gid}&single=true&output=csv"
     r = s.get(google_sheet_url)
-    assert (r.status_code == 200), "Something went wrong with PDF download"
+    assert (r.status_code == 200), "Something went wrong with download"
     filename = f"{sheet_name}.csv"
     download_path = DIR_DATA/ filename
     with open(download_path, 'wb') as f:
