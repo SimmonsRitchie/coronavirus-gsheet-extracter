5ba80b09965c6e37fa397a0aaf9961e6988046f6
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 16:04:15 -0400

    Fix: Add pyenv shell command

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Cronjob script
 
+# Start
+echo "Start cronjob"
+# Start pyenv shell
+pyenv shell 3.6.10
 # This tells pipenv to use this .env file
 export PIPENV_DOTENV_LOCATION="/home/dansr/projects/coronavirus_gs_extracter/.env"
 # Navigate to scraper project directory
 cd /home/dansr/projects/coronavirus_gs_extracter/
 # This is needed to ensure pipenv runs when used from cron
-export PYENV_VERSION="3.6.10"
 export PATH=~/.pyenv/shims:~/.pyenv/bin:"$PATH"
 # Run program
 pipenv run python coronavirus_gs_extracter.py
