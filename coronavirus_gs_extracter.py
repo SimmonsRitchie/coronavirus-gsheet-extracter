5c0024b9c51b5f96147011dacb4ba97b4c46bbf5
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 11:13:14 -0400

    Change google sheet ID from testing to prod

--- coronavirus_gs_extracter.py
+++ coronavirus_gs_extracter.py
@@ -3,7 +3,7 @@ from coronavirus_gs_extracter.download_sheet import download_sheet
 from coronavirus_gs_extracter.move_to_s3 import move_to_s3
 from coronavirus_gs_extracter.clean import clean_data
 def main():
-    sheet_id = "2PACX-1vTSjpwdfT574IFWdhyn8ZUCAOyDr2kHMfzBJLC8qMeCrsE0fl4NfN9LFk9E_GunrKYWM5baIpTbF_nv"
+    sheet_id = "2PACX-1vScoEuwTZAdLCbmWaoDsxRJcZSovzW-HI8UXlK7LqF-4FVr07pIARzCWoy0xjtlf0Wa5p1U0ZBEVnLQ"
     google_sheets = [
         {
             "name": "cases",
