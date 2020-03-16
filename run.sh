a15e02493132512b0d66ffaebe0e28952ab69f78
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 15:01:42 -0400

    Fix: path error

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # This tells pipenv to use this .env file
 export PIPENV_DOTENV_LOCATION="/home/projects/dansr/coronavirus_gs_extracter/.env"
 # Navigate to scraper project directory
-cd /home/projects/dansr/coronavirus_gs_extracter/
+cd /home/dansr/projects/coronavirus_gs_extracter/
 # This is needed to ensure pipenv runs when used from cron
 PATH=/usr/local/bin:$PATH
 # Run program
