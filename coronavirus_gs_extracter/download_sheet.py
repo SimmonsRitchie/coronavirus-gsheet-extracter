954c0adf79c52900c33c962e80ee21d188415a8e
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-04-03 10:28:31 -0400

    Reformat:

--- coronavirus_gs_extracter/download_sheet.py
+++ coronavirus_gs_extracter/download_sheet.py
@@ -8,8 +8,8 @@ def download_sheet(sheet_name, sheet_id, gid, output_filename):
     s = requests.Session()
     google_sheet_url = f"https://docs.google.com/spreadsheets/d/e/{sheet_id}/pub?gid={gid}&single=true&output=csv"
     r = s.get(google_sheet_url)
-    assert (r.status_code == 200), "Something went wrong with download"
-    download_path = DIR_DATA/ output_filename
-    with open(download_path, 'wb') as f:
+    assert r.status_code == 200, "Something went wrong with download"
+    download_path = DIR_DATA / output_filename
+    with open(download_path, "wb") as f:
         f.write(r.content)
-        logging.info(f'{sheet_name} sheet downloaded as: {output_filename}')
+        logging.info(f"{sheet_name} sheet downloaded as: {output_filename}")
